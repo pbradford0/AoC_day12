afa60a96ee0d03c2497375ac033c2cdfa02c6964
pbradford0 <phil.w.bradford@gmail.com>
2016-11-30 22:24:01 -0500

    initialize

---
+++ day12.py
@@ -0,0 +1,16 @@
+#Author: Phil Bradford
+#Solution for http://adventofcode.com/2015/day/12
+
+def main():
+  if len(sys.argv) != 2:
+    print 'Please specify an input file'
+    sys.exit(1)
+
+  #a = pass_change(sys.argv[1])
+  #print "Santa's new password is: " + str(a)
+  
+  #b = longest_route(sys.argv[1])
+  #print "The longest route is distance" + str(b)
+
+if __name__ == '__main__':
+  main()

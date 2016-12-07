00fb3c6ebede53d4c27327ef170edb14ddd05e93
pbradford0 <phil.w.bradford@gmail.com>
2016-12-06 20:39:38 -0500

    part 1 done

--- day12.py
+++ day12.py
@@ -1,16 +1,41 @@
 #Author: Phil Bradford
 #Solution for http://adventofcode.com/2015/day/12
 
+import sys
+import re
+import json
+
+def red_math(filename):
+  total = 0
+  re_numbers = re.compile('(-?\d+)')
+  re_object = re.compile('{.*}')
+  re_red = re.compile(r':\"red\"')
+  #parse all numbers not within { }
+  input = open(filename, 'rU')
+  parsed_input = json.load(input)
+  parsed_input.values()
+  return total
+
+def elf_math(filename):
+  total = 0
+  re_numbers = re.compile('(-?\d+)')
+  #parse all numbers from a file and add them together
+  input = open(filename, 'rU').read()
+  numbers = re.findall(re_numbers, input)
+  for x in numbers:
+    total += int(x)
+  return total
+
 def main():
   if len(sys.argv) != 2:
     print 'Please specify an input file'
     sys.exit(1)
 
-  #a = pass_change(sys.argv[1])
-  #print "Santa's new password is: " + str(a)
+  #a = elf_math(sys.argv[1])
+  #print "Elf math: " + str(a)
   
-  #b = longest_route(sys.argv[1])
-  #print "The longest route is distance" + str(b)
+  b = red_math(sys.argv[1])
+  print "Elf math: " + str(b)
 
 if __name__ == '__main__':
   main()
